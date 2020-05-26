e72346bde9ab1e1af011f5361692c31d58290f4e
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-05-26 10:21:05 +0700

    ubah reward menjadi poin

--- modules/Mission/resources/views/_form.blade.php
+++ modules/Mission/resources/views/_form.blade.php
@@ -1,6 +1,6 @@
 {!! form()->text('title')->label('Title') !!}
 {!! form()->redactor('description')->label('Description') !!}
-{!! form()->rupiah('reward')->label('Reward') !!}
+{!! form()->input('reward')->prependIcon('coins')->label('Reward Point') !!}
 {!! form()->dropdown('level', \App\Enums\Level::toSelectArray())->label('Level') !!}
 {!! form()->datepicker('due_date')->label('Due Date') !!}
 {!! form()->action([
