e32bb880fe47dd05a6a5f5a007d837b912e07312
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-05-27 12:29:30 +0700

    build

---
+++ resources/views/vendor/laravolt/components/inspire.blade.php
@@ -0,0 +1,5 @@
+<img src="{{ asset('img/fly.svg') }}" alt="" class="ui image centered">
+<div class="ui divider hidden section"></div>
+<div style="font-size: 1.5rem">
+    {{ \Illuminate\Foundation\Inspiring::quote() }}
+</div>
