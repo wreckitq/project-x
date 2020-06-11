89ffce97795e35c1ad991c63c02d7ad53cd7509c
Laravel Shift <shift@laravelshift.com>
2020-06-11 01:57:07 +0000

    Generate HTTP tests for controllers

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class LoginTest extends TestCase
 {
     use RefreshDatabase;
 
